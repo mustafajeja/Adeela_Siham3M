3b973b0921b01292f946c2d07d50c99777f993a3
MufasaAdam <mustafajeja@gmail.com>
2018-07-28 10:07:25 +0200

    Update Side Nav

--- app/src/main/java/com/siham_3m/adeeela/MainActivity.java
+++ app/src/main/java/com/siham_3m/adeeela/MainActivity.java
@@ -80,17 +80,21 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.account) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.booking) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.transaction) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.salespoint) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.help) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.aboutUs) {
+
+        } else if (id == R.id.contactUs) {
+
+        } else if (id == R.id.lang) {
 
         }
 
