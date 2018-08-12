6afa06a856c2aa9aac55a3764a7c97041b42e50b
MufasaAdam <mustafajeja@gmail.com>
2018-08-12 11:33:12 +0200

    Add page BusSeat49, Update Confirmation Page

--- app/src/main/java/com/siham_3m/adeeela/busSeats45.java
+++ app/src/main/java/com/siham_3m/adeeela/busSeats45.java
@@ -1,8 +1,11 @@
 package com.siham_3m.adeeela;
 
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class busSeats45 extends AppCompatActivity {
